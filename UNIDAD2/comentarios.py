7846f8ea8bd057aaf2dfee7cf10b49ad85aebc03
Di3g0-ctrl <L251050183@pabellon.tecnm.mx>
2025-09-17 08:02:01 -0600

    Create comentarios.py

---
+++ UNIDAD2/comentarios.py
@@ -0,0 +1,13 @@
+"""Este programa imprime la variable. También observamos la mutabilidad de python"""
+
+entero = 10
+flotante = 3.14
+cadena = "Diego Emiliano"
+booleana = True
+
+print(entero)
+print(flotante)
+
+
+"""Diego Emiliano
+ITPA"""
