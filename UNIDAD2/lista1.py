8796e0d1f8e31b4f8ee225e08f72c21048bcc2dd
Di3g0-ctrl <L251050183@pabellon.tecnm.mx>
2025-09-18 08:02:23 -0600

    Create lista1.py

---
+++ UNIDAD2/lista1.py
@@ -0,0 +1,16 @@
+"""#Se realiza una lista con variables y un print que se encarga de imprimir la lista """
+lista = ["Juan", "Ivan", "Adrian", "Alice"]
+print (lista)
+"""#Esta linea de código imprime la variable de la posicion indicada"""
+print (lista [2])
+"""#Agrega un elemento a la lista"""
+lista.append("Alexa")
+print (lista)
+#Insetra una variable a la lista y en la posicion indicada y lo imprime
+lista.insert(1, "Javier")
+lista.pop()
+print (lista)
+#Quita un elemnto de la lista y lo imprime
+lista.remove("Juan")
+print (lista)
+lista.pop(1)
