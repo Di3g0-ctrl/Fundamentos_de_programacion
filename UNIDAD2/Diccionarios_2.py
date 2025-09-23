c23077f6ca661add2d41a72369d7f8f44e52a0b7
Di3g0-ctrl <L251050183@pabellon.tecnm.mx>
2025-09-23 07:49:24 -0600

    Update Diccionarios_2.py

--- UNIDAD2/Diccionarios_2.py
+++ UNIDAD2/Diccionarios_2.py
@@ -2,27 +2,27 @@ import json
 
 Biblioteca = {
     "978-84-376-0494-7": {
-        "Título": "Cien años de soledad",
+        "Título": ["Cien años de soledad"],
         "Autor": ["Gabriel García Márquez"],
         "Géneros": ["Realismo mágico", "Novela histórica"]
     },
     "978-84-204-1625-5": {
-        "Título": "Don Quijote de la Mancha",
+        "Título": ["Don Quijote de la Mancha"],
         "Autor": ["Miguel de Cervantes Saavedra"],
         "Géneros": ["Novela de caballería", "Satira"]
     },
       "978-84-9889-839-9": {
-        "Título": "El nombre del viento",
+        "Título": ["El nombre del viento"],
         "Autor": ["Patrick Rothfuss"],
         "Géneros": ["Fantasía épica"]
     },
       "978-84-08-03793-3": {
-        "Título": "La sombra del viento",
+        "Título": ["La sombra del viento"],
         "Autor": ["Carlos Ruiz Zafón"],
         "Géneros": ["Misterio", "Novela histórica contemporánea"]
     },  
       "978-0-06-112008-4": {
-        "Título": "Matar a un ruiseñor",
+        "Título": ["Matar a un ruiseñor"],
         "Autor": ["Harper Lee"],
         "Géneros": ["Novela clásica", "Drama social"]
     }
